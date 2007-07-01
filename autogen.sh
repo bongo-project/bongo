8aa49d112b5cde1c5f32f47171c13630de35e5ea
alexhudson <alexhudson@52461e19-6027-0410-9301-c4706890ca92>
2007-07-01 21:17:50 +0000

    Quick hack to make the build work, but that we should later revert: i bug #9427 - po/ Make system should be turned into Bongo.rules

--- autogen.sh
+++ autogen.sh
@@ -454,7 +454,9 @@ conf_flags="--enable-maintainer-mode"
 if test x$NOCONFIGURE = x; then
     printbold Running $srcdir/configure $conf_flags "$@" ...
     $srcdir/configure $conf_flags "$@" \
-	&& echo Now type \`make\' to compile $PKG_NAME || exit 1
+	|| exit 1
+    `cd po/ && make update-gmo 2>/dev/null`
+    echo Now type \`make\' to compile $PKG_NAME 
 else
     echo Skipping configure process.
 fi
