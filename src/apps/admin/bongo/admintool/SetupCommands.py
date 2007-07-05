956885a7686a72b645f16f71144549eab015c752
trisk <trisk@52461e19-6027-0410-9301-c4706890ca92>
2007-07-05 19:55:03 +0000

    Fix exception in bongo-setup if eclients.dat does not already exist (sorry). Remove existing file for safety.

--- src/apps/admin/bongo/admintool/SetupCommands.py
+++ src/apps/admin/bongo/admintool/SetupCommands.py
@@ -119,7 +119,12 @@ class SetupCredentialCommand(Command):
         omask = os.umask(0077)
 
         eclients = os.path.join(Xpl.DEFAULT_DBF_DIR, "eclients.dat")
-        os.chmod(eclients, 0600)
+
+        try:
+            os.remove(eclients)
+        except os.error:
+            pass
+
         fd = open(eclients, "w")
         fd.write(passwd)
         fd.write("\0")
