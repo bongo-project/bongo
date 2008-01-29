4d1e47dcad24bed61bc8eaad04f90df31baaf95e
alexhudson <alexhudson@52461e19-6027-0410-9301-c4706890ca92>
2008-01-29 10:18:56 +0000

    Remove legacy bongo-setup application

--- src/apps/setup/bongo/__init__.py
+++
@@ -1,7 +0,0 @@
-# THIS FILE SHOULD NOT BE INSTALLED!
-
-# It exists only to merge the src/[...]/bongosetup/bongo and src/libs/python/bongo
-# directories in the Bongo source tree together.  In an installed
-# system, they're going to be in the same place.
-
-__path__.append(__path__[0] + "/../../../libs/python/bongo")
