ed453ef28703b7b5cbdc5e92956e8f108621426d
jonnylamb <jonnylamb@52461e19-6027-0410-9301-c4706890ca92>
2008-02-17 16:13:21 +0000

    Potential fix for iCal 3.0. See comments regarding undocumented GET behaviour for a word on what's going on.

--- src/www/bongo/sundial/GetView.py
+++ src/www/bongo/sundial/GetView.py
@@ -32,24 +32,68 @@ class GetHandler(SundialHandler):
     def do_GET(self, req, rp):
         store = StoreClient(req.user, rp.user, authPassword=req.get_basic_auth_pw())
 
-        # Get the nmap document from the store.
-        try:
-            doc = store.Read('/events/%s' % rp.filename)
-        except:
-            return bongo.commonweb.HTTP_NOT_FOUND
+        # The CalDAV spec does not mention what to do with a GET request on a
+        # collection. However, looking at the DAViCal GET source, it seems that
+        # the client is just after an iCal download for the whole collection.
 
-        jsob = bongojson.ParseString(doc.strip())
+        # So, if there is a filename, act normally as the RFC specifies:
+        if rp.filename is not None:
+            # Get the nmap document from the store.
+            try:
+                doc = store.Read('/events/%s' % rp.filename)
+            except:
+                return bongo.commonweb.HTTP_NOT_FOUND
 
-        if jsob:
-            # TODO TODO TODO This should not be encoded into ASCII at all.
-            # At the moment, this doesn't work without though.
-            data = cal.JsonToIcal(jsob).encode('ascii', 'replace')
+            jsob = bongojson.ParseString(doc.strip())
+
+            if jsob:
+                # TODO TODO TODO This should not be encoded into ASCII at all.
+                # At the moment, this doesn't work without though.
+                data = cal.JsonToIcal(jsob).encode('ascii', 'replace')
+            else:
+                data = ""
+
+            # Get rid of the boring headers, and data.
+            req.headers_out["Content-Length"] = str(len(data))
+            req.content_type = 'text/calendar'
+
+            req.write(data)
+            return bongo.commonweb.HTTP_OK
+
         else:
-            data = ""
+            # A collection has had a GET request on it. So, return all the events
+            # in the collection.
 
-        # Get rid of the boring headers, and data.
-        req.headers_out["Content-Length"] = str(len(data))
-        req.content_type = 'text/calendar'
+            # Get calender GUID from name.
+            try:
+                info = store.Info("/calendars/" + rp.calendar)
+                caluid = info.uid
+            except:
+                return bongo.commonweb.HTTP_NOT_FOUND
 
-        req.write(data)
-        return bongo.commonweb.HTTP_OK
+            # Get the events from the store.
+            events = list(store.Events(caluid, ["nmap.document", "nmap.event.calendars"], start=None, end=None))
+
+            output = ''
+
+            for event in events:
+                # Get NMAP document for current VEVENT.
+                doc = event.props['nmap.document'].strip()
+
+                # Parse the document into JSON.
+                jsob = bongojson.ParseString(doc)
+
+                if jsob:
+                    # Convert JSON to iCalendar format.
+                    # TODO FixASCII encoding. See above.
+                    calendardata = cal.JsonToIcal(jsob).encode('ascii', 'replace')
+                else:
+                    calendardata = ''
+
+                output += calendardata
+
+            req.headers_out['Content-Length'] = str(len(output))
+            req.content_type = 'text/calendar'
+
+            req.write(output)
+            return bongo.commonweb.HTTP_OK
